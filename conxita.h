397feecc7543801ac65619aacbca71a1ae492735
Nuria Ferron Ferriz <nuferron@car1s4.42barcelona.com>
2023-10-02 14:26:45 +0200

    pre-\.h

---
+++ conxita.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <curses.h>
