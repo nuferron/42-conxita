32a31cded490f1e5cab8e554f312313c5a80c510
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-10-03 20:08:55 +0200

    heredoc fully working hehe

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:17:38 by nuferron          #+#    #+#             */
-/*   Updated: 2023/10/03 16:57:36 by nuferron         ###   ########.fr       */
+/*   Updated: 2023/10/03 20:07:07 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,28 @@
 int	getting_doc(char *key)
 {
 	char	*line;
-	char	*result;
-	char	*tmp;
+	int		fd_pipe[2];
 
 	line = readline("> ");
-	tmp = NULL;
+	if (pipe(fd_pipe) == -1)
+		return (-1);
 	while (line)
 	{
 		if (ft_strlen(key) == ft_strlen(line)
 			&& ft_strncmp(line, key, ft_strlen(key) - 1) == 0)
 			break ;
-		result = ft_strjoin(tmp, line);
+		write(fd_pipe[1], &line, ft_strlen(line));
 		free(line);
-		free(tmp);
-		tmp = result;
-		result = ft_strjoin(tmp, "\n");
-		free(tmp);
-		tmp = result;
 		line = readline("> ");
 	}
 	free(line);
-	printf("%s", result);
 	return (0);
 }
 
 int	here_doc(char *key)
 {
-	int	fd_pipe[2];
 
-	if (pipe(fd_pipe) == -1)
-		return (-1);
-	if (dup2(fd_pipe[1], 1))
+	getting_doc(key);
 	return (0);
 }
 
