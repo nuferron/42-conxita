a27d890c333ab306a5990441a66705dc93fb2ddf
BlaiVO <blaivilarnauolid@gmail.com>
2023-10-03 15:13:39 +0200

    gitignore + more stuff idk

--- src/prompt_handler.c
+++ src/prompt_handler.c
@@ -6,7 +6,7 @@
 /*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:23:30 by blvilarn          #+#    #+#             */
-/*   Updated: 2023/10/03 12:25:46 by blvilarn         ###   ########.fr       */
+/*   Updated: 2023/10/03 15:03:41 by blvilarn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,25 @@
 
 int	handle_prompt(char *prompt)
 {
-	if (!prompt || !ft_strncmp(prompt, "", 2))
+	if (!prompt)
+	{
+		rl_replace_line("conxita$ exit", 0);
+		exit(0);
+	}
+	if (!ft_strncmp(prompt, "", 2))
+	{
+		free(prompt);
 		return (0);
+	}
 	add_history(prompt);
 	if (!ft_strncmp(prompt, "exit", 5))
 	{
 		free(prompt);
 		exit(0);
 	}
-	if (prompt)
+	if (!ft_strncmp(prompt, "conxita", 8))
+		print_conxita();
+	else if (prompt)
 		printf("%s\n", prompt);
 	free (prompt);
 	return (0);
