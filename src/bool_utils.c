499514d0a4b1001997ffd60f02a461458cfcb097
BlaiVO <blaivilarnauolid@gmail.com>
2023-10-03 20:16:13 +0200

    _get_quotes done (needs to be renamed and put to actual use)

--- src/bool_utils.c
+++ src/bool_utils.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   conxita.c                                          :+:      :+:    :+:   */
+/*   bool_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/02 12:48:44 by blvilarn          #+#    #+#             */
-/*   Updated: 2023/10/03 19:17:20 by blvilarn         ###   ########.fr       */
+/*   Created: 2023/10/03 17:48:06 by blvilarn          #+#    #+#             */
+/*   Updated: 2023/10/03 19:26:47 by blvilarn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../conxita.h"
 
-int	main(void)
+void	b_invert(bool *b)
 {
-	char	*prompt;
-
-	print_conxita();
-	setup_signals();
-	while ("Conxita")
-	{
-		prompt = readline("conxita$ ");
-		handle_prompt(prompt);
-	}
-	return (0);
+	*b = !*b;
 }
