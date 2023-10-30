d8d2eb798ec91512c615af936daa3b3596b55c11
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-10-30 23:52:24 +0100

    added / changed print_errors, not final version!

--- src/errors/errors.c
+++ src/errors/errors.c
@@ -1,7 +1,10 @@
 #include "../../conxita.h"
 
-int	print_error()
+int	print_errors(char *str)
 {
+	if (str)
+		printf("conxita: %s %s\n", SYNTAX, str);
+		return (258);
 	perror("conxita");
 	return (-1);
 }
