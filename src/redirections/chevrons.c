781ba8e9b937af87f829254968704d3ee4272ebd
nuferron <138325463+nuferron@users.noreply.github.com>
2023-11-03 20:43:22 +0000

    created strjoin with 3 strings

--- src/redirections/chevrons.c
+++ src/redirections/chevrons.c
@@ -4,11 +4,25 @@
   before the execution function*/
 /*must be changed so its information is saved in a special
   place (it shouldn't be overwritten)*/
+
+char	*ft_strjoin3_free(char *str1, char *str2, char *str3)
+{
+	char	*tmp;
+	char	*result;
+
+	tmp = ft_strjoin(str1, str2);
+	free(str1);
+	free(str2);
+	result = ft_strjoin(tmp, str3);
+	free(str3);
+	free(tmp);
+	return (result);
+}
+
 int	here_doc(t_redir *redir, char *key)
 {
 	char	*line;
 	char	*tmp;
-	char	*aux;
 
 	line = readline("> ");
 	tmp = NULL;
@@ -17,14 +31,9 @@ int	here_doc(t_redir *redir, char *key)
 		if (ft_strlen(key) == ft_strlen(line)
 			&& ft_strncmp(line, key, ft_strlen(key) - 1) == 0)
 			break ;
-		aux = ft_strjoin(tmp, line);
-		free(line);
-		if (tmp)
-			free(tmp);
-		tmp = aux;
-		aux = ft_strjoin(tmp, "\n");
-		free(tmp);
-		tmp = aux;
+		tmp = ft_strjoin3_free(line, tmp, "\n");
+		if (!tmp)
+			return (print_errors(NULL));
 		line = readline("> ");
 	}
 	write(redir->fd_pipe[1], tmp, ft_strlen(tmp));
