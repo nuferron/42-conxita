32b8a1dcc44d766429bb15fc32b3d35f693085eb
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-11-13 20:26:32 +0100

    should be working now

--- src/utils/struct_conversion.c
+++ src/utils/struct_conversion.c
@@ -24,13 +24,13 @@ int	arg_len(t_oken *token, int i)
 	return (i);
 }
 
-int	init_chev_input(t_oken *token, t_cmd *cmd, int i)
+int	init_chev_input(t_oken *token, t_cmd *cmd, int *i)
 {
-	if (token[i].val[1] == '<') // eof
+	if (token[*i].val[1] == '<') // eof
 	{
-		if (token[i + 1].val)
+		if (token[*i + 1].val)
 		{
-			cmd->heredoc = ft_strdup(token[i + 1].val);
+			cmd->heredoc = ft_strdup(token[*i + 1].val);
 			if (!cmd->heredoc)
 				return (print_errors(NULL));
 			cmd->input = heredoc;
@@ -38,21 +38,22 @@ int	init_chev_input(t_oken *token, t_cmd *cmd, int i)
 	}
 	else
 	{
-		cmd->infile = ft_strdup(token[i].val);
+		cmd->infile = ft_strdup(token[*i].val);
 		if (!cmd->infile)
 			return (print_errors(NULL)); // Needs proper call to a proper error function
 		cmd->input = infile;
 	}
-	if (!token[i + 1].val)
+	if (!token[*i + 1].val)
 		exit(print_errors("\'newline\'")); // It's a little bit guarro but bueno
+	(*i)++;
 	return (0);
 }
 
-int	init_chev_output(t_oken *token, t_cmd *cmd, int i)
+int	init_chev_output(t_oken *token, t_cmd *cmd, int *i)
 {
 	int	fd;
 
-	cmd->outfile = ft_strdup(token[i + 1].val);
+	cmd->outfile = ft_strdup(token[*i + 1].val);
 	if (!cmd->outfile)
 		return (print_errors(NULL));
 	fd = open(cmd->outfile, O_WRONLY | O_CREAT, 0664);
@@ -60,12 +61,13 @@ int	init_chev_output(t_oken *token, t_cmd *cmd, int i)
 		return (print_errors(NULL));
 	if (close(fd) == -1)
 		return (print_errors(NULL));
-	if (token[i].val[1] == '>' && token[i + 1].val)
+	if (token[*i].val[1] == '>' && token[*i + 1].val)
 		cmd->output = f_append;
-	else if (token[i].val[1] == '\0' && token[i + 1].val)
+	else if (token[*i].val[1] == '\0' && token[*i + 1].val)
 		cmd->output = f_trunc;
-	if (!token[i + 1].val)
+	if (!token[*i + 1].val)
 		exit(print_errors("\'newline\'")); // It's a little bit guarro but bueno
+	(*i)++;
 	return (0);
 }
 
@@ -77,23 +79,24 @@ void	init_pipe(t_cmd *cmd, int is_pipe)
 		cmd->output = opipe;
 }
 
-int	init_cmd(t_oken *token, t_cmd *cmd, int i)
+int	init_cmd(t_oken *token, t_cmd *cmd, int *i)
 {
 	int			ret;
 	static int	is_pipe = 0;
 
 	ret = 0;
-	if (token[i].val[0] == '<')
+	if (token[*i].val[0] == '<')
 		ret = init_chev_input(token, cmd, i);
-	else if (token[i].val[0] == '>')
+	else if (token[*i].val[0] == '>')
 		ret = init_chev_output(token, cmd, i);
-	else if (token[i].val[0] == '|' || is_pipe == 1)
+	else if (token[*i].val[0] == '|' || is_pipe == 1)
 	{
 		init_pipe(cmd, is_pipe);
 		is_pipe = 1;
 	}
 	else
 		is_pipe = 0;
+	(*i)++;
 	return (ret);
 }
 
@@ -110,7 +113,8 @@ int	init_arg(t_oken *token, t_cmd *cmd, int i)
 	int	j;
 
 	j = 0;
-	cmd->cmd = (char **)malloc(sizeof(char *) * (arg_len(token, i) + 1));
+	if (!cmd->cmd)
+		cmd->cmd = (char **)malloc(sizeof(char *) * (arg_len(token, i) + 2));
 	if (!cmd->cmd)
 		return (print_errors(NULL));
 	while (token[i].val && token[i].type == arg)
@@ -121,13 +125,12 @@ int	init_arg(t_oken *token, t_cmd *cmd, int i)
 			cmd->cmd = free_matrix(cmd, j);
 			return (print_errors(NULL));
 		}
-		printf("init arg\tcmd->cmd[%d] = %s\n", j, cmd->cmd[j]);
 		j++;
 	}
 	cmd->cmd[j] = NULL;
 	return (i);
 }
-
+/*
 t_cmd	*token_to_cmd(t_oken *token) // not norminetted
 {
 	t_cmd	*cmd;
@@ -155,21 +158,56 @@ t_cmd	*token_to_cmd(t_oken *token) // not norminetted
 		}
 		if (token[i].type == arg)
 		{
-			i = init_arg(token, &cmd[j], i);
+			i = init_arg(token, &cmd[j], i++);
 			if (i == -1)
 				return (NULL); // free stuff
 		}
 		if (token[i].val && token[i].val[0] == '|')
 			j++;
-		printf("t->c cmd: %s\n", cmd[j - 1].cmd[0]);
-		i++;
+		printf("\033[1;36mt->c cmd: %s\tj = %d\n", cmd[j].cmd[0], j);
 	}
 	printf("t->c cmd: %s\n", cmd[0].cmd[0]);
-	exit(printf("\033[1;31mexit in token to cmd\033]0m\n"));
+	exit(printf("\033[1;31mexit in token to cmd\033[0m\n"));
+	return (cmd);
+}*/
+
+t_cmd	*token_to_cmd(t_oken *token) // not norminetted
+{
+	t_cmd	*cmd;
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	len = arg_len(token, 0) + 1;
+	cmd = malloc(sizeof(t_cmd) * len);
+	if (!cmd)
+	{
+		print_errors(NULL);
+		return (NULL); // Needs proper call to a proper error function
+	}
+	while (j < len)
+	{
+		if (token[i].type == red)
+		{
+			init_cmd(token, &cmd[j], &i);
+			if (token[i - 1].val[0] != '|')
+				i += 2;
+		}
+		if (token[i].type == arg)
+		{
+			i = init_arg(token, &cmd[j], i);
+			if (i == -1)
+				return (NULL); // free stuff
+		}
+		if (!token[i].val || (token[i].val && token[i].val[0] == '|'))
+			j++;
+	}
 	return (cmd);
 }
-/*
-int	main()
+
+/*int	main()
 {
 	t_oken	*token;
 	t_cmd	*cmd;
@@ -201,10 +239,10 @@ int	main()
 	cmd = token_to_cmd(token);
 
 	int	i = 0;
-	int	j = token_len(token);
-	printf("main pre while |%s|\n", cmd[i].cmd[0]);
+	int	j = arg_len(token, 0);
+	printf("\n\nmain pre while |%s|\n", cmd[i].cmd[0]);
 	int	k = 0;
-	while (i < j)
+	while (i <= j)
 	{
 		k = 0;
 		while (cmd[i].cmd[k])
@@ -212,7 +250,7 @@ int	main()
 			printf("main cmd[%d] = %s\n", i, cmd[i].cmd[k]);
 			k++;
 		}
-		printf("\n");
+		printf("main output %s\n", cmd[i].outfile);
 		i++;
 	}
 	exit(1);
