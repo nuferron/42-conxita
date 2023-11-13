ff8bf4454c26a685f00a5760800b4475abda27a4
blai8 <blai@blai8s-MacBook-Air.local>
2023-11-13 17:33:27 +0100

    fixed compilation error

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: blai <blai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:44:20 by blvilarn          #+#    #+#             */
-/*   Updated: 2023/11/07 18:11:22 by blvilarn         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:32:59 by blai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 //Prompt tokenizer
 t_oken	*glorified_ft_split(char *prompt, char **env)
 {
-	int		i;
+	//int		i;
 	int		token_num;
 	t_oken	*tokens;
 
-	i = 0;
+	//i = 0;
 	prompt = ft_strtrim(expand_env(env, prompt), " ");
-	printf("%s\n", prompt);
+	//printf("%s\n", prompt);
 	token_num = get_token_num(prompt);
 	tokens = ft_calloc(token_num + 1, sizeof(t_oken));
 	if (!tokens)
