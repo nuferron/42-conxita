12351fd6740b7d6598dbf58e5c653a08ab075922
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-11-15 13:25:33 +0100

    added functions to convert env to t_env

--- src/utils/env_utils.c
+++ src/utils/env_utils.c
@@ -16,14 +16,11 @@ int	path_count(const char *s, char c)
 	return (counter + 1);
 }
 
-char	*search_env(char **env, char *key)
+char	*search_env(t_env *env, char *key)
 {
-	int	i;
-
-	i = 0;
-	while (env[i] && ft_strncmp(key, env[i], ft_strlen(key)))
-		i++;
-	return (env[i]);
+	while (env && ft_strncmp(key, env->key, ft_strlen(key)))
+		env = env->next;
+	return (env->value);
 }
 
 int	len_to_char(char *str, char c)
@@ -62,3 +59,71 @@ char	*mini_split(char *path, int	count)
 	free(tmp);
 	return (result);
 }
+
+int	mat_len(char **mat)
+{
+	int	i;
+
+	i = 0;
+	while (mat && mat[i])
+		i++;
+	return (i);
+}
+
+char	*splitting_env(char *env, int flag)
+{
+	char	*str;
+	int		equal;
+
+	equal = 0;
+	while (env[equal] != '=' && env[equal] != '\0')
+		equal++;
+	if (flag == 0)
+	{
+		env[equal] = '\0';
+		str = env;
+	}
+	else
+		str = &env[equal + 1];
+	return (str);
+}
+
+t_env	*env_to_lst(char **sys_env)
+{
+	t_env	*env;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = mat_len(sys_env);
+	env = (t_env *)malloc(sizeof(t_env) * len);
+	if (!env)
+		return (NULL); // future me: error management is missing!
+	while (i < len)
+	{
+		env[i].key = ft_strdup(splitting_env(sys_env[i], 0));
+		env[i].value = ft_strdup(splitting_env(sys_env[i], 1));
+		if (i > 0)
+			env[i - 1].next = &env[i];
+		env[i].next = NULL;
+		i++;
+	}
+	return (env);
+}
+/*
+int	main(int argc, char **argv, char **env)
+{
+	t_env *tenv;
+
+	(void)argc;
+	(void)argv;
+	tenv = env_to_lst(env);
+	if (!tenv)
+		return (-1);
+	while (tenv)
+	{
+		printf("key: %s\tvalue: %s\n", tenv->key, tenv->value);
+		tenv = tenv->next;
+	}
+	return (0);
+}*/
