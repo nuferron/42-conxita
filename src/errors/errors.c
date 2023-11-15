b35ec9335a60f501a9fc22a92b1d276962e67e29
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-11-15 20:04:49 +0100

    added brackets so it doesn't return syntax error even when str = NULL

--- src/errors/errors.c
+++ src/errors/errors.c
@@ -3,8 +3,11 @@
 int	print_errors(char *str)
 {
 	if (str)
-		printf("conxita: %s %s\n", SYNTAX, str);
-		return (258);
+	{
+		printf("conxita: %s \033[1;36m%s\033[0m\n", SYNTAX, str);
+		//return (258);
+		return (-1);
+	}
 	perror("conxita");
 	return (-1);
 }
