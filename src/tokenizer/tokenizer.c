9f4323bc95960b7d0b51adc53ae3bfbdbc6b3815
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-11-29 23:51:10 +0100

    replaced some returns with exits

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -8,13 +8,13 @@ t_oken	*glorified_ft_split(char *prompt, t_env *env)
 
 	prompt = ft_strtrim_free(expand_env(env, prompt), " ");
 	if (!prompt)
-		return (NULL);
+		exit((unsigned char)print_errors(NULL));
 	token_num = get_token_num(prompt);
 	if (token_num == 0)
 		return (NULL);
 	tokens = generate_tokens(prompt, token_num);
 	free (prompt);
 	if (!tokens)
-		return (NULL);
+		exit((unsigned char)print_errors(NULL));
 	return (tokens);
 }
