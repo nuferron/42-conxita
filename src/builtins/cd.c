cda5f3c39e31674c8e7c0d317d64452173d0ee42
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-11-29 18:02:25 +0100

    functional and working properly

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,22 +1,41 @@
 #include "../../conxita.h"
 
-int	builtin_cd(char **args)
+char	*go_home(t_env *env)
+{
+	char	*user;
+
+	user = search_env(env, "USER");
+	if (!user)
+		return (NULL);
+	user = ft_strjoin("/Users/", user);
+	if (!user)
+		exit((unsigned char)print_errors(NULL));
+	return (user);
+}
+
+int	builtin_cd(char **args, t_env *env)
 {
 	int		argn;
+	char	*home;
 
 	argn = get_arg_number(args);
 	if (argn > 1)
 	{
-		printf("cd: too many arguments");
-		return (1);
+		ft_dprintf(2, "conxita: cd: too many arguments\n");
+		return (-1);
 	}
 	if (argn == 0)
 	{
-		if (chdir('~') == -1)
-			return (1);
+		home = go_home(env);
+		if (chdir(home) == -1)
+		{
+			ft_dprintf(2, "conxita: cd: HOME not set\n");
+			return (-1);
+		}
+		free(home);
 		return (0);
 	}
-	if (chdir(args[0] == -1))
-		return (1);
+	if (chdir(args[0]) == -1)
+		return (print_errors("cd"));
 	return (0);
 }
