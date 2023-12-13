549c4704b9e25692d71f993b694a4e77b7a25c2e
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2023-12-13 19:03:47 +0100

    ctrl C in heredoc solved

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -6,7 +6,7 @@
 /*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:36:42 by nuferron          #+#    #+#             */
-/*   Updated: 2023/12/13 18:52:25 by nuferron         ###   ########.fr       */
+/*   Updated: 2023/12/13 19:02:03 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	redirections(t_cmd *cmd, t_redir *redir)
 	close(redir->fd_pipe[1]);
 	if (cmd->chev && init_chev(cmd->chev, cmd->last) == -1)
 		return (-1);
+	if (cmd->chev->type == here && cmd->last[0] == -1)
+		exit(1);
 	if (cmd->last[0] > 0 && dup2(cmd->last[0], 0) == -1)
 		exit(-print_errors(NULL));
 	if (cmd->last[1] != -1 && dup2(cmd->last[1], 1) == -1)
@@ -46,10 +48,7 @@ int	init_chev(t_chev *chev, int *last)
 		if ((chev->type == outt || chev->type == outa) && last[1] < 0)
 			return (print_errors(chev->file));
 		if (chev->type == here)
-		{
 			last[0] = chev->fd;
-			printf("init chev: here %d\n", last[0]);
-		}
 		else if (chev->type == in)
 			last[0] = open(chev->file, O_RDONLY);
 		if (chev->type == in && last[0] < 0)
